892649a3bee7f803fc5ee200e835ed210f301f35
David Martinez <david.mtz00@hotmail.com>
2019-05-19 12:01:21 -0500

    Updates

---
+++ precache-manifest.c13f2e0c6987cff1f7dbab9a4964da24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5c8623c47108db09857e4267aba21a",
+    "url": "/vision_zero/index.html"
+  },
+  {
+    "revision": "71d6d08740b207e674b5",
+    "url": "/vision_zero/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bdb2707f6f5bae4dee5c",
+    "url": "/vision_zero/static/js/2.83e7268c.chunk.js"
+  },
+  {
+    "revision": "71d6d08740b207e674b5",
+    "url": "/vision_zero/static/js/main.29e22b1e.chunk.js"
+  },
+  {
+    "revision": "0ed16df0d282c2e5ca7c",
+    "url": "/vision_zero/static/js/runtime~main.e525feba.js"
+  },
+  {
+    "revision": "a66425a06ddd2d2c3122fc1cd88e95e1",
+    "url": "/vision_zero/static/media/placeholderHoustonMap.a66425a0.png"
+  },
+  {
+    "revision": "f8421277c54b7c18c50c02eab6ed0568",
+    "url": "/vision_zero/static/media/upturnedCar.f8421277.jpeg"
+  }
+]);
